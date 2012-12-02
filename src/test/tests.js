53b43507f14ce7bf5849952e1e0ed3fb2abc0ab2
Taylor Clifton <taylor.john.clifton@gmail.com>
2012-12-02 00:04:47 -0500

    test stuff

--- src/test/tests.js
+++ src/test/tests.js
@@ -1,25 +1,5 @@
 !function($) {
   var compiler = new Compiler();
-
-  function generateYapl() {
-    function traverse(ruleName) {
-      var result = [];
-
-      if (Grammar[ruleName]) {
-        var productions = Grammar[ruleName];
-        var randomIndex = Math.floor((Math.random() * (productions.length)));
-
-        var production = productions[randomIndex];
-
-        for (var i = 0; i < production.length; ++i) {
-          result.push(traverse(production[i]));
-        }
-      }
-    }
-
-    return traverse('Program');
-  };
-
   test('test it all', function() {
 
   });
