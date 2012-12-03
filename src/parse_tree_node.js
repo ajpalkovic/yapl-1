9a4649b362c3c89ec85788b59b7a6458251e31ef
Taylor Clifton <taylor.john.clifton@gmail.com>
2012-12-02 21:41:21 -0500

    parse tree node

--- src/parse_tree_node.js
+++ src/parse_tree_node.js
@@ -2,26 +2,23 @@
   var Node = klass({
     initialize: function Node(type) {
       this.type = type;
-      this.namedChildren = {};
       this.children = [];
       this.parent = undefined;
     },
 
+    is: function(type) {
+      return this.type === type;
+    },
+
     append: function(child, name) {
       this.children.append(child);
-      if (name) this.namedChildren[name] = child;
+      if (name) this[name] = child;
+
+      child.parent = this;
     },
 
     clone: function() {
-      var clonedNode = new Node(this.type);
-      var namedChildren = $.extend({}, this.namedChildren);
-
-      for (var name in namedChildren) {
-        var child = namedChildren[name].clone();
-        clonedNode.append(child);
-      }
-
-      return clonedNode;
+      return $.extend({}, this);
     }
   });
 };
